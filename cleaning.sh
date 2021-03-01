f59ca84e928e699e92a7ff0cd3058990fa005df5
yanbrasiliano <yanpenabr@gmail.com>
2021-03-01 19:52:45 -0300

    Update cleaning.sh

--- cleaning.sh
+++ cleaning.sh
@@ -7,6 +7,37 @@
 #
 #
 
-echo 'Update System...'
-sleep(1)
+#Atualizar repositórios.
+echo 'Update System...'; sleep 1
 sudo apt update
+echo
+#Upgrade nos pacotes que forem solicitados.
+echo 'Upgrade System...'; sleep 1
+sudo apt upgrade
+echo 
+#Remover libs e pacotes obseletos e não mais utilizados.
+echo 'Autoremove Unsed Packages ...'; sleep 1
+sudo apt autoremove -y
+sudo apt autoremove --purge -y
+echo 
+echo 'Cleaning Local Repository...'; sleep 1
+sudo apt autoclean
+echo 
+echo 'General Cleaning of the system...'; sleep 1
+sudo rm -vfr ~/.thumbnails/normal/*
+sleep 1
+sudo du -sh /var/cache/apt/archives/ 
+sleep 1
+sudo rm -f ~/.cache/thumbnails/normal/*
+sleep 1
+sudo apt clean
+sleep 1
+sudo rm -rf /var/tmp/*
+sleep 1
+sudo rm -rf "${HOME}/.local/share/Trash/"*
+sleep 1
+sudo du -sh /var/cache/apt/archives/
+sleep 1
+sudo dpkg --configure -a
+echo
+
