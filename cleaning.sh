d5fa5ea6cefe9342e0ba4e25ad4a72d85635f55f
yanbrasiliano <yanpenabr@gmail.com>
2021-03-01 19:33:33 -0300

    Update cleaning.sh

---
+++ cleaning.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+#
+#cleaning.sh - script para automaitizar a limpeza de cache,logs e arquivos no GNU-LINUX.
+#Autor:			Yan B.S.Penalva <yanpenabr@gmail.com>
+#
+#------------------------------------------------------
+#
+#
+
+echo 'Update System...'
+sleep(1)
+sudo apt update
