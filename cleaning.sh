9aa0f20cdc3ae14fc307df139e650da8500f36da
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2023-05-03 21:06:29 -0300

    Update cleaning.sh

--- cleaning.sh
+++ cleaning.sh
@@ -4,12 +4,7 @@
 LOG_FILE="$HOME/update.log"
 THUMBNAILS_DIR="$HOME/.thumbnails/normal"
 SLEEP_TIME=1
-
-# Check for root privileges
-if [ "$(id -u)" -ne 0 ]; then
-    echo "This script must be run as root."
-fi
-
+JUMP_LINE=echo
 
 prepare_logfile () {
   echo "----------[ $(whoami) $(date) ]----------" >> "$LOG_FILE"
@@ -18,69 +13,87 @@ prepare_logfile () {
 update_system () {
   echo 'Updating system...'
   sudo apt update -y
+  $JUMP_LINE
   echo 'Upgrading packages...'
   sudo apt upgrade -y
   sudo apt full-upgrade -y
+  $JUMP_LINE
   echo 'Removing unused packages...'
   sudo apt autoremove --purge -y
+  $JUMP_LINE
   echo 'Cleaning local repository...'
   sudo apt autoclean
-
+  
+  $JUMP_LINE
+  
   echo 'Cleaning system...'
   sudo rm -rf "$THUMBNAILS_DIR"/*
   sudo du -sh "$THUMBNAILS_DIR"
   sleep "$SLEEP_TIME"
-
+  
+  $JUMP_LINE
   sudo rm -rf /var/cache/apt/archives/
   sudo du -sh /var/cache/apt/archives/
   sleep "$SLEEP_TIME"
-
+  
+  $JUMP_LINE
   sudo rm -rf /var/cache/apt/archives/*deb
   sudo du -sh /var/cache/apt/archives/*deb
   sleep "$SLEEP_TIME"
 
+  $JUMP_LINE
   sudo rm -f ~/.cache/thumbnails/normal/*
   sudo du -sh ~/.cache/thumbnails/normal/*
   sleep "$SLEEP_TIME"
-
+  
+  $JUMP_LINE
   sudo apt clean
   sleep "$SLEEP_TIME"
 
+  $JUMP_LINE
   sudo rm -rf /var/tmp/*
   sudo du -sh /var/tmp/*
   sleep "$SLEEP_TIME"
 
+  $JUMP_LINE
   sudo rm -rf "$HOME/.local/share/Trash/"*
   sudo du -sh "$HOME/.local/share/Trash/"*
   sleep "$SLEEP_TIME"
-
+  
+  $JUMP_LINE
   echo 'Fixing broken packages with dpkg...'
   sudo dpkg --configure -a
 
+  $JUMP_LINE
   echo 'Cleaning old logs in /var/log...'
   sudo journalctl --vacuum-size=50M
   sudo rm -rf /var/log/*gz /var/log/*1 /var/log/*old*
   sudo du -sh /var/log
   sleep "$SLEEP_TIME"
 
+  $JUMP_LINE
   echo 'Cleaning old backups in /var/backups...'
   sudo rm -rf /var/backups/*gz
   sudo du -sh /var/backups/
   sleep "$SLEEP_TIME"
-
+  
+  $JUMP_LINE
   echo 'Cleaning cache in /home...'
   sudo rm -rf ~/.cache/*
   sudo du -sh ~/.cache/
   sleep "$SLEEP_TIME"
 
+  $JUMP_LINE
   echo 'Removing old kernels...'
   sudo apt remove --purge -y $(dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p')
   sleep "$SLEEP_TIME"
 
+  $JUMP_LINE
   echo 'Removing old configuration files...'
   sudo dpkg -l | grep '^rc' | awk '{print $2}' | xargs -r sudo dpkg --purge
   sleep "$SLEEP_TIME"
 
+  $JUMP_LINE
   echo 'Removing unnecessary files from home directory...'
   rm -rf ~/Downloads/*
   rm -rf ~/.cache/*
@@ -88,6 +101,7 @@ update_system () {
   rm -rf ~/.thumbnails/*
   sleep "$SLEEP_TIME"
   
+  $JUMP_LINE
   # Remove old snaps
   set -eu
   snap list --all | awk '/disabled/{print $1, $3}' |
@@ -95,6 +109,8 @@ update_system () {
         sudo snap remove "$snapname" --revision="$revision"
     done
     
+  $JUMP_LINE
+  $JUMP_LINE
   # List all partitions size
   df -Th | sort
 }
